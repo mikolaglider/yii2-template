ee01890d88036dddeb09dd2b0ab7761421684cff
mikolaglider <30018819+mikolaglider@users.noreply.github.com>
2023-11-30 14:33:18 +0300

    Common DI setup

---
+++ common/bootstrap/DISetup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace common\bootstrap;
+
+use yii\base\BootstrapInterface;
+
+class DISetup implements BootstrapInterface
+{
+    public function bootstrap($app): void
+    {
+
+    }
+}
